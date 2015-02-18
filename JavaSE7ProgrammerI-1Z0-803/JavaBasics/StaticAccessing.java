3b8ac297c50027662cbba9eb43dd630e43c9debb
Franze Jr <franzejr@gmail.com>
2015-02-18 20:11:30 -0300

    Static variable accessing

---
+++ JavaSE7ProgrammerI-1Z0-803/JavaBasics/StaticAccessing.java
@@ -0,0 +1,10 @@
+class StaticAccessing {
+    static int i = 3;
+
+    public static void main(String[] a) {
+            for (new StaticAccessing().i = 10; new Teste().i < 100;
+                                 new StaticAccessing().i++) {
+                        System.out.println(i);
+                    }
+        }
+}
