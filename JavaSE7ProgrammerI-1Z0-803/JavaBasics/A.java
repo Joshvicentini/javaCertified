1353531d68e0db7478a70cad047005f3c7ba367b
Franze Jr <franzejr@gmail.com>
2015-02-18 21:40:42 -0300

    String args

---
+++ JavaSE7ProgrammerI-1Z0-803/JavaBasics/A.java
@@ -0,0 +1,7 @@
+class A {
+    public static void main(String[] args) {
+            System.out.println(args);
+            System.out.println(args.length);
+            System.out.println(args[0]);
+        }
+}
